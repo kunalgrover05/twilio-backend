ccc717202ccc452bd314dbb4b144b853b5a6ddd9
Kunal Grover <kunalgrover05@gmail.com>
2019-01-26 18:31:00 +0530

    Filters for Sent SMS and Responded

--- core/admin.py
+++ core/admin.py
@@ -12,14 +12,11 @@ from core.models import Customer, SMS, Tag, MessageTemplate
 from django.contrib import admin
 
 
-class CustomerFilter(admin.SimpleListFilter):
-
+class CustomerContactListFilter(admin.SimpleListFilter):
     title = 'Contact List exists?'
-
     parameter_name = 'list_exists'
 
     def lookups(self, request, model_admin):
-
         return (
             ('yes', 'Yes'),
             ('no',  'No'),
@@ -28,15 +25,30 @@ class CustomerFilter(admin.SimpleListFilter):
     def queryset(self, request, queryset):
         if self.value() == 'yes':
             return queryset.filter(contact_list__isnull=False).exclude(contact_list='')
-
         if self.value() == 'no':
             return queryset.filter(Q(contact_list__isnull=True) | Q(contact_list__exact=''))
 
 
+class CustomerSentSMSFilter(admin.SimpleListFilter):
+    title = 'Sent SMS?'
+    parameter_name = 'sent_sms'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Yes'),
+            ('no',  'No'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'yes':
+            return queryset.filter(first_sms__isnull=False)
+        if self.value() == 'no':
+            return queryset.filter(first_sms__isnull=True)
+
 
 class CustomerAdmin(admin.ModelAdmin):
     search_fields = ('name', )
-    list_filter = ('contact_list', CustomerFilter)
+    list_filter = ('contact_list', CustomerContactListFilter, CustomerSentSMSFilter, 'responded')
 
     class Meta:
         model = Customer
